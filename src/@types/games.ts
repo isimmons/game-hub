a51fbc15dd67fc15bb5d3b7ba32c6e5328afbff6
isimmons <isimmons33@gmail.com>
2023-04-01 16:46:48 -0400

    create and display platform icons

--- src/@types/games.ts
+++ src/@types/games.ts
@@ -2,6 +2,13 @@ export type Game = {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: GamePlatform }[];
+};
+
+export type GamePlatform = {
+  id: number;
+  name: string;
+  slug: string;
 };
 
 export type FetchGamesResponse = {
