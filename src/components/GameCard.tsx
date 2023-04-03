33c466886b1cf56f10c831d1e40348adb5cb9534
isimmons <isimmons33@gmail.com>
2023-04-02 20:51:50 -0400

    clean up game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,10 +1,8 @@
-import { Card, CardBody, Heading, HStack, Image, Text } from '@chakra-ui/react';
+import { Card, CardBody, Heading, HStack, Image } from '@chakra-ui/react';
 
 import CriticScore from './CriticScore';
 import PlatformIconList from './PlatformIconList';
 import getCroppedImageUrl from '~/services/image-url';
-
-import type { Game } from '~/@types';
 
 type Props = {
   game: Game;
@@ -17,14 +15,14 @@ const GameCard = ({ game }: Props) => {
     <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize="2xl">{title}</Heading>
         {subTitle && <Heading fontSize="1xl">{subTitle}</Heading>}
-        <HStack justifyContent="space-between">
+        <HStack justifyContent="space-between" marginBottom={3}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize="2xl">{title}</Heading>
       </CardBody>
     </Card>
   );
