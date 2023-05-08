2a74c6f1af25c1c0a1eb7ceb02be490b6ca886cf
isimmons <isimmons33@gmail.com>
2023-05-08 01:45:40 -0400

    setup react-query and devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,23 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App';
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import theme from './theme';
 import './index.css';
 
+const isDev = process.env.NODE_ENV === 'development' ? true : false;
+const qc = new QueryClient();
+
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
-    </ChakraProvider>
+    <QueryClientProvider client={qc}>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <App />
+        {isDev && <ReactQueryDevtools />}
+      </ChakraProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
