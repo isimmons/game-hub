0ae6775a4184db0ba4cdf31c83c2cf7f2d9dd4d6
isimmons <isimmons33@gmail.com>
2023-05-08 03:55:49 -0400

    Refactor: fetch games with react-query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,17 +1,20 @@
-import useData from './useData';
+import { useQuery } from '@tanstack/react-query';
+import apiClient from '~/services/api-client';
 
 const useGames = (gameQuery: GameQuery) =>
-  useData<Game>(
-    '/games',
-    {
-      params: {
-        genres: gameQuery.genre?.id,
-        parent_platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText,
-      },
-    },
-    [gameQuery]
-  );
+  useQuery<FetchResponse<Game>, Error>({
+    queryKey: ['games', gameQuery],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Game>>('/games', {
+          params: {
+            genres: gameQuery.genre?.id,
+            parent_platforms: gameQuery.platform?.id,
+            ordering: gameQuery.sortOrder,
+            search: gameQuery.searchText,
+          },
+        })
+        .then((res) => res.data),
+  });
 
 export default useGames;
