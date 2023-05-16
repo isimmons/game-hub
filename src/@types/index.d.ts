d9c34f37649ed5721f19a940115b2ec8f65c16f4
isimmons <isimmons33@gmail.com>
2023-05-16 04:22:04 -0400

    build game attributes

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -4,10 +4,17 @@ type Platform = {
   slug: string;
 };
 
+type Publisher = {
+  id: number;
+  name: string;
+};
+
 type Game = {
   id: number;
   name: string;
   slug: string;
+  genres: Genre[];
+  publishers?: Publisher[];
   description_raw: string;
   background_image: string;
   parent_platforms?: { platform: Platform }[];
